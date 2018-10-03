507611793fcbe6271cd9663704ff6581ecfc30c3
medmunds <medmunds@gmail.com>
2018-10-03 10:34:03 -0700

    Split SES modifications from DNS generation; output all tokens
    * Simplifies some more-complex Update cases
    * Allows handling Delete as an Update to neither send nor receive
    * Include outputs allowing caller to generate their own DNS records
      or handle other scenarios
    * Improve formatting for ZoneFileEntries

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# See also:
-# - https://medium.com/poka-techblog/verify-domains-for-ses-using-cloudformation-8dd185c9b05c
-# - https://github.com/binxio/cfn-ses-provider
+# AWS Lambda handler implementing Amazon SES domain identity provisioning
+# for use as a CloudFormation CustomResource
+
 import json
 import logging
 import os
@@ -16,11 +16,11 @@ logger.setLevel(os.getenv("LOG_LEVEL", "WARNING"))
 
 
 DEFAULT_PROPERTIES = {
-    "Domain": None,
+    "Domain": "",
     "EnableSend": True,
     "EnableReceive": False,
     "MailFromSubdomain": "mail",
-    "DMARC": '"v=DMARC1; p=none; pct=100; sp=none; aspf=r;"',
+    "CustomDMARC": '"v=DMARC1; p=none; pct=100; sp=none; aspf=r;"',
     "TTL": "1800",
     "Region": os.getenv("AWS_REGION"),
 }
@@ -34,116 +34,150 @@ def lambda_handler(event, context):
 
     properties = DEFAULT_PROPERTIES.copy()
     properties.update(event["ResourceProperties"])
-    domain = properties["Domain"]
     logger.info("Expanded properties to %r", properties)
 
-    # Validate inputs
-    if not domain:
-        send(event, context, FAILED,
-             reason="The 'Domain' property is required.",
-             physical_resource_id="MISSING")
-        return
-
+    # Clean and validate inputs
     try:
-        if event["RequestType"] == "Delete":
-            handle_delete(domain)
-            records = []
-        else:
-            records = handle_create_or_update(domain, properties)
+        properties["Domain"] = properties["Domain"].strip().rstrip(".")
+    except (TypeError, ValueError):
+        pass
+    domain = properties["Domain"]
+
+    if not domain:
+        return send(event, context, FAILED,
+                    reason="The 'Domain' property is required.",
+                    physical_resource_id="MISSING")
+
+    if event["RequestType"] == "Delete":
+        # Treat Delete as a request to disable both directions
+        properties["EnableSend"] = False
+        properties["EnableReceive"] = False
+
+    # Update SES
+    try:
+        outputs = update_ses_domain_identity(domain, properties)
     except (BotoCoreError, ClientError) as error:
         # for ClientError, might be helpful to look at error.response, too
-        logger.exception("Error processing request: %s", error)
-        send(event, context, FAILED, reason=str(error), physical_resource_id=domain)
-    else:
-        outputs = {
-            "Route53RecordSets": records,
-            "ZoneFileEntries": [record_to_zone_file(record) for record in records],
-        }
-        send(event, context, SUCCESS, response_data=outputs, physical_resource_id=domain)
+        logger.exception("Error updating SES: %s", error)
+        return send(event, context, FAILED,
+                    reason=str(error), physical_resource_id=domain)
+
+    # Determine required DNS
+    properties.update(outputs)
+    route53_records = generate_route53_records(domain, properties)
+    outputs.update({
+        "Domain": domain,
+        "Route53RecordSets": route53_records,
+        "ZoneFileEntries": route53_to_zone_file(route53_records),
+    })
+
+    return send(event, context, SUCCESS,
+                response_data=outputs, physical_resource_id=domain)
 
 
-def handle_delete(domain):
-    """De-provision SES domain identity"""
-    ses.delete_identity(Identity=domain)
-    logger.info("SES:DeleteIdentity(Identity=%r) succeeded", domain)
-
-
-def handle_create_or_update(domain, properties):
-    """Provision SES domain and return list of AWS::Route53::RecordSet resources required"""
-    records = []
-
+def update_ses_domain_identity(domain, properties):
+    """Handle SES (de-)provisioning for domain and returns dict of output info"""
+    outputs = {}
     enable_send = properties["EnableSend"]
     enable_receive = properties["EnableReceive"]
 
     if enable_send or enable_receive:
-        token = ses.verify_domain_identity(Domain=domain)["VerificationToken"]
-        logger.info("SES:VerifyDomainIdentity(Domain=%r) returned %r", domain, token)
+        response = ses.verify_domain_identity(Domain=domain)
+        logger.info("SES:VerifyDomainIdentity(Domain=%r) => %r", domain, response)
+        outputs["VerificationToken"] = response["VerificationToken"]
+    else:
+        # Neither send nor receive, so de-provision
+        response = ses.delete_identity(Identity=domain)
+        logger.info("SES:DeleteIdentity(Identity=%r) => %r", domain, response)
+
+    if enable_send:
+        response = ses.verify_domain_dkim(Domain=domain)
+        logger.info("SES:VerifyDomainDKIM(Domain=%r) => %r", domain, response)
+        # ??? ses.set_identity_dkim_enabled(Identity=domain, DkimEnabled=True)
+        outputs["DkimTokens"] = response["DkimTokens"]
+
+    if enable_send and properties["MailFromSubdomain"]:
+        mail_from_domain = "{MailFromSubdomain}.{Domain}".format(**properties)
+        outputs.update({
+            "MailFromDomain": mail_from_domain,
+            "MailFromMX": "feedback-smtp.{Region}.amazonaws.com".format(**properties),
+            "MailFromSPF": '"v=spf1 include:amazonses.com -all"',
+        })
+    else:
+        # Disable custom Mail FROM domain.
+        # (Could check first using ses.get_identity_mail_from_domain,
+        # but clearing it doesn't cause an error even if not set/applicable.)
+        mail_from_domain = ""
+    response = ses.set_identity_mail_from_domain(Identity=domain, MailFromDomain=mail_from_domain)
+    logger.info("SES:SetIdentityMailFromDomain(Domain=%r, MailFromDomain=%r) => %r",
+                domain, mail_from_domain, response)
+
+    if enable_send and properties["CustomDMARC"]:
+        outputs["DMARC"] = properties["CustomDMARC"]
+
+    if enable_receive:
+        outputs.update({
+            "ReceiveMX": "inbound-smtp.{Region}.amazonaws.com".format(**properties),
+        })
+
+    return outputs
+
+
+def generate_route53_records(domain, properties):
+    """Return list of AWS::Route53::RecordSet resources required"""
+    records = []
+
+    if properties.get("VerificationToken"):
         records.append({
             "Name": "_amazonses.{Domain}.".format(**properties),
             "Type": "TXT",
-            "ResourceRecords": ['"{token}"'.format(token=token)]
-        })
+            "ResourceRecords": ['"{VerificationToken}"'.format(**properties)]})
 
-    if enable_send:
-        dkim_tokens = ses.verify_domain_dkim(Domain=domain)['DkimTokens']
-        logger.info("SES:VerifyDomainDKIM(Domain=%r) returned %r", domain, dkim_tokens)
-        # ??? ses.set_identity_dkim_enabled(Identity=domain, DkimEnabled=True)
+    if properties.get("DkimTokens"):
         records.extend([{
             "Name": "{token}._domainkey.{Domain}.".format(token=token, **properties),
             "Type": "CNAME",
-            "ResourceRecords": ["{token}.dkim.amazonses.com".format(token=token)]
-        } for token in dkim_tokens])
+            "ResourceRecords": ["{token}.dkim.amazonses.com.".format(token=token)],
+        } for token in properties["DkimTokens"]])
 
-        if properties["MailFromSubdomain"]:
-            mail_from_domain = "{MailFromSubdomain}.{Domain}".format(**properties)
-            mail_from_domain_fq = "{}.".format(mail_from_domain)
-            ses.set_identity_mail_from_domain(Identity=domain, MailFromDomain=mail_from_domain)
-            logger.info("SES:SetIdentityMailFromDomain(Domain=%r, MailFromDomain=%r) succeeded",
-                        domain, mail_from_domain)
-            records.extend([{
-                "Name": mail_from_domain_fq,
-                "Type": "MX",
-                "ResourceRecords": [
-                    "10 feedback-smtp.{Region}.amazonaws.com.".format(**properties)
-                ]
-            }, {
-                "Name": mail_from_domain_fq,
-                "Type": "TXT",
-                "ResourceRecords": ['"v=spf1 include:amazonses.com -all"']
-            }])
-
-        if properties["DMARC"]:
+    if properties.get("MailFromDomain"):
+        if properties.get("MailFromMX"):
             records.append({
-                "Name": domain,
+                "Name": "{MailFromDomain}.".format(**properties),
+                "Type": "MX",
+                "ResourceRecords": ["10 {MailFromMX}.".format(**properties)]})
+        if properties.get("MailFromSPF"):
+            records.append({
+                "Name": "{MailFromDomain}.".format(**properties),
                 "Type": "TXT",
-                "ResourceRecords": [properties["DMARC"]]
-            })
+                "ResourceRecords": [properties["MailFromSPF"]]})
 
-    if not enable_send or not properties["MailFromSubdomain"]:
-        # (could check with ses.get_identity_mail_from_domain first, but no harm in clearing if it's not set)
-        ses.set_identity_mail_from_domain(Identity=domain, MailFromDomain="")
-        logger.info("SES:SetIdentityMailFromDomain(Domain=%r, MailFromDomain=%r) succeeded", domain, "")
-
-    if enable_receive:
+    if properties.get("DMARC"):
         records.append({
-            "Name": domain,
+            "Name": "{Domain}.".format(**properties),
+            "Type": "TXT",
+            "ResourceRecords": [properties["DMARC"]]})
+
+    if properties.get("ReceiveMX"):
+        records.append({
+            "Name": "{Domain}.".format(**properties),
             "Type": "MX",
-            "ResourceRecords": [
-                "10 inbound-smtp.{Region}.amazonaws.com.".format(**properties)
-            ]
-        })
+            "ResourceRecords": ["10 {ReceiveMX}.".format(**properties)]})
 
     for record in records:
         record["TTL"] = properties["TTL"]
-
     return records
 
 
-def record_to_zone_file(record):
-    """Return a Zone File line for an AWS::Route53::RecordSet"""
-    return "{name}\t{ttl}\tIN\t{type}\t{data}".format(
-        name=record["Name"], ttl=record["TTL"], type=record["Type"],
-        data=" ".join(record["ResourceRecords"]))
+def route53_to_zone_file(records):
+    """Return a list of Zone File lines from a list of AWS::Route53::RecordSet"""
+    max_name_len = max([len(record["Name"]) for record in records], default=1)
+    return [
+        "{name:{max_name_len}}\t{ttl}\tIN\t{type:5}\t{data}".format(
+            name=record["Name"], max_name_len=max_name_len,
+            ttl=record["TTL"], type=record["Type"],
+            data=" ".join(record["ResourceRecords"]))
+        for record in records]
 
 
 #
@@ -172,6 +206,9 @@ def send(event, context, response_status, reason=None, response_data=None, physi
                       headers={"Content-Type": "application/json"})
     try:
         response = urlopen(request)
-        logger.info("Successfully sent response: status=%s reason=%s", response.status, response.reason)
     except HTTPError as exc:
         logger.exception("Error sending response: code=%s", exc.code)
+        return False
+    else:
+        logger.info("Successfully sent response: status=%s reason=%s", response.status, response.reason)
+        return True
