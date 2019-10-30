fd56cfdc693761574b97952e7a42d2925624dcc9
Mike Edmunds <medmunds@gmail.com>
2019-10-30 16:34:37 -0700

    Move to GitHub
    Change scripts and links to treat GitHub as primary repository for this project (previously on GitLab).
    
    Closes #2
    
    * Bump version
    * Change links from GitLab to GitHub
    * Add changelog
    * Upload assets to GitHub releases

--- aws_cfn_ses_domain/__about__.py
+++ aws_cfn_ses_domain/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018 Michael V. Edmunds
+# Copyright 2019 Michael V. Edmunds
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this software except in compliance with the License.
@@ -14,12 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (0, 2)
+VERSION = (0, 3, 'dev0')
 __version__ = ".".join(str(v) for v in VERSION)
 
 NAME = "aws-cfn-ses-domain"
 DESCRIPTION = "AWS CloudFormation custom resource for managing Amazon SES domains"
-HOMEPAGE = "https://gitlab.com/medmunds/aws-cfn-ses-domain"
+HOMEPAGE = "https://github.com/medmunds/aws-cfn-ses-domain"
 AUTHOR = "Mike Edmunds"
 AUTHOR_EMAIL = "medmunds@gmail.com"
 LICENSE = "Apache License 2.0"
